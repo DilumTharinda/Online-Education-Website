0463d7a6c5eb42d26fd9af245c0e621895599aac
DilumTharinda <tharindadilum6@gmail.com>
2025-05-06 19:53:54 +0530

    Added EduWeb project files: HTML, CSS, and JS

---
+++ activity.js
@@ -0,0 +1,88 @@
+
+const navbar = document.querySelector('.NavigationBar');
+const menu = document.querySelector('#MenuBtn');
+
+menu.onclick = () =>{ // same to if method but can't use if here
+
+menu.classList.toggle('fa-times');
+navbar.classList.toggle('AndroidBoy_active');
+}
+
+
+//swiper
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
+
+  //-------------------
+  
+  var swiper = new Swiper(".ourteacher", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+    },
+  });
+
+  //------------------------------------
+
+  var swiper = new Swiper(".StudentsReviews", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+    },
+  });
+
+
+//auto update year
+  document.getElementById('year').textContent = new Date().getFullYear();
